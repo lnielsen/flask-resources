a855492cc0b08e83c445dea31bcf773d6a55e582
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-04-28 16:24:53 +0200

    Comments

--- invenio_resources/controller.py
+++ invenio_resources/controller.py
@@ -17,6 +17,20 @@ from invenio_search import RecordsSearch
 from .search import es_search_factory
 from .config import lt_es7, obj_or_import_string
 
+
+class SearchFactory:
+    def create(query, ctx):
+
+    def filters()
+
+
+    def aggregations()
+
+
+    def query_parser()
+        #...
+
+
 # TODO: Is there a generic InvenioResourceController class needed?
 class RecordController:
     """Invenio record resource controller."""
@@ -29,7 +43,7 @@ class RecordController:
         record_class=Record,
         indexer_class=RecordIndexer,
         search_class=RecordsSearch,
-        search_factory=es_search_factory,
+        search_factory=SearchFactory,
     ):
         """Contructor."""
         self.pid_minter_name = pid_minter_name
@@ -39,8 +53,8 @@ class RecordController:
         self.pid_name = pid_type_name  # TODO: Needed?
         self.record_class = record_class
         self.indexer_class = indexer_class
-        self.search_class = search_class
-        self.search_factory = obj_or_import_string(search_factory)
+        # self.search_class = search_class
+        self.search_factory = SearchFactory
 
     def minter(self, rec_uuid, data):
         """Lazy loads the minter."""
@@ -58,6 +72,17 @@ class RecordController:
 
         return self._fetcher(rec_uuid)
 
+    @cached_property
+    def resolver(self):
+        return Resolver()
+
+    @permission_required('search')
+    def search(self, query=None, from_idx=None, to_idx=None, ctx=None):
+        #...
+        search = self.search_factory(ctx=ctx.user_needs)
+        # We need a wrapper to somehow serialise back out the result. Like the marshmallow schema should be used for this.
+        return SearchResult(search.execute()
+
     def search(self, pagination, query):
         """Perform a search over the items."""
         search_obj = self.search_class()
@@ -68,18 +93,26 @@ class RecordController:
         if not lt_es7:
             search_action = search_action.extra(track_total_hits=True)
 
+        # Lars: Can we avoid this extra factory?
         search_action = self.search_factory(
             search=search_action, query_string=query
         )
 
         # Execute search
+        # Lars: we need to return a proper representation
         return search_action.execute()
 
-    def create(self, data):
+    @permission_required('search')
+    def create(self, data, index=True, ctx=None):
         """Create a record."""
+        # Lars: We need a marshmallow schema here that does business logic data validation.
+        # - this schema should be used for both records and deposits.
+        data = self.schema_cls().load(data)
+
         # Create uuid for record
         record_uuid = uuid.uuid4()
         # Create persistent identifier
+        # Lars: in schema or outside schema.
         pid = self.minter(record_uuid, data=data)
         # Create record
         record = self.record_class.create(data, id_=record_uuid)
@@ -92,7 +125,9 @@ class RecordController:
 
         return record
 
-    def read(self, rec_uuid):
+    @resolve('id')
+    @permission_required('read')
+    def read(self, pid, record):
         """Read an item."""
         # TODO
         return None
@@ -100,3 +135,18 @@ class RecordController:
     def delete(self, rec_uuid):
         """Delete an item."""
         return {"status": "Accepted"}
+
+## usage
+ctrl = RecordController()
+record  = ctrl.create(
+    dict(title=..)
+    ctx={user_needs=current_identity.provies}
+)
+
+ctx = dict(needs='current_identity.provides')
+record = ctrl.read('axcfd-acdf', ctx=ctx)
+record.data['title'] = 'test'
+ctrl.update(record)
+
+record = ctrl.create(Record(data={})) #???
+
